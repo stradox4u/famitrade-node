f7d71631658f440d42f0e7eb0ab1c1de8de2aeb5
Odeh Adejoh <42589643+stradox4u@users.noreply.github.com>
2021-08-05 01:44:30 +0100

    setup a postgres db and connected it to the app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('dotenv').config()
 
+const sequelize = require('./util/database')
+
 const port = process.env.APP_PORT
 
 const app = express()
@@ -9,5 +11,12 @@ app.get('/', (req, res, next) => {
   console.log('App Working!')
   return res.json({ message: 'App is Online!' })
 })
+
+try {
+  return sequelize.authenticate()
+    .then(result => console.log('Connection to database successful!'))
+} catch (err) {
+  console.log('Unable to connect to database')
+}
 
 app.listen(port)
