615907415b8bb809ca200be1e634274140f8b867
Odeh Adejoh <42589643+stradox4u@users.noreply.github.com>
2021-08-19 15:48:50 +0100

    created isVerified middleware

---
+++ src/middleware/isVerified.js
@@ -0,0 +1,22 @@
+const db = require('../../models')
+
+module.exports = async (req, res, next) => {
+  try {
+    const user = await db.user.findOne({ where: { id: req.user.id } })
+
+    if (!user) {
+      const error = new Error('User not found!')
+      error.statusCode = 404
+      throw error
+    }
+
+    if (!user.email_verified_at) {
+      const error = new Error('Email not verified')
+      error.statusCode = 401
+      throw error
+    }
+    next()
+  } catch (err) {
+    next(err)
+  }
+}
